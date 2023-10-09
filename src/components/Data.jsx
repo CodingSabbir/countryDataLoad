55737be732635d921be9d3fb9151f3ed9a660012
sabbir <mdsabbirrahmanm740@gmail.com>
2023-10-09 14:18:10 +0600

    facke data load

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Country from "./Country";
 import Card from "./Card";
+import facedata from "./Data.json"
 
 
 const Data = () => {
     const [countris,setCountris]=useState([])
     const [card,setCard]=useState([])
-    console.log(card)
-
+    const [data,setData]=useState([])
+    // console.log(card)
+    console.log(data)
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
         .then(res=>res.json())
@@ -19,6 +21,10 @@ const Data = () => {
        
         })
     },[]);
+
+ useEffect(()=>{
+    setData(facedata)
+ },[])
    
     const addCountry =(country)=>{
         const newCard=[...card,country]
@@ -27,20 +33,33 @@ const Data = () => {
  
     return (
         <>
-             <h1 className="font-bold text-center mt-5"> country name:{countris.length}</h1>
-               <h2 className="text-center">Add Country:{card.length}</h2>
-             <Card addCard={card}/>
-                <ul>
-                {
-                    countris.map((cntName)=>(
-                    //    console.log(cntName)
-                       <Country handleAddCountry={addCountry} country= {cntName} key={cntName.timezones}/>
-                       
-                    ))
-                }
-                </ul>
-      
+        <div>
+            <h1 className="font-bold text-center mt-5"> country name:{countris.length}</h1>
+                <h2 className="text-center">Add Country:{card.length}</h2>
+                <Card addCard={card}/>
+            
+                    <ul>
+                    {
+                        countris.map((cntName)=>(
+                        //    console.log(cntName)
+                        <Country handleAddCountry={addCountry} country= {cntName} key={cntName.timezones}/>
+                        
+                        ))
+                    }
+                    </ul>
 
+                    {
+                       
+                        data.map((dataItems,i)=>(
+                            <div className="border p-5 m-5" key={i}> 
+                             <h1 className="text-[30px] font-mono">Team Builder</h1>
+                            <h1> <span className="font-semibold">Email :</span> {dataItems.email}</h1>
+                            <p> <span className="font-semibold">First Name :</span> {dataItems.first_name}</p>
+                            </div>
+                        ))
+                    }
+        </div> 
+            
         </>
     );
 };
